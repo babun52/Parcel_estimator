629c70b6a5b11122840acc2bc0d91e1804bebf91
Diego Suarez and James Lee <diego@>
2016-11-28 11:11:08 -0800

    Check the cost of ship based on distance.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,17 +1,20 @@
 class Parcel
-  define_method(:initialize) do |length, width, height, maxDays|
+
+  define_method(:initialize) do |length, width, height, maxDays, distance|
     @length = length
     @width = width
     @height = height
     @maxDays = maxDays
+    @distance = distance
   end
+
   define_method(:volume) do
     @length * @width * @height
   end
   define_method(:cost) do
-    (volume * (1.6 - (@maxDays * 0.05)) * 0.4).round(2)
+    (volume * (1.6 - (@maxDays * 0.05)) * (0.99 + (@distance * 0.01)) * 0.6).round(2)
   end
 end
-
-
-# (volume * (1.6 - (maxDays * 0.05)) * 0.4)
+# a = Parcel.new(:length=>3,:width=>3,:height=>3,:maxDays=>20)
+# puts(a.cost())
+# (volume * (1.6 - (maxDays * 0.05)) * (.99 + (distance * 0.01) * 0.6)
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,11 +3,16 @@ require('parcels')
 
 describe('Parcel#check_price') do
   it('calculate the volume') do
-    new_parcel = Parcel.new(3,3,3,20)
+    new_parcel = Parcel.new(3,3,3,20,1)
     expect(new_parcel.volume()).to(eq(27))
   end
   it('calculate the cost of the ship based on speed of delivery') do
-    new_parcel = Parcel.new(3,3,3,5)
-    expect(new_parcel.cost()).to(eq(14.58))
+    new_parcel = Parcel.new(3,3,3,5,1)
+    expect(new_parcel.cost()).to(eq(21.87))
   end
+  it('calculate the cost of ship based on distance') do
+    new_parcel = Parcel.new(3,3,3,5,20)
+    expect(new_parcel.cost()).to(eq(26.03))
+  end
+
 end
