18311426a03f3dc4c94d1a3b906afd9dd91ea9b9
Diego Suarez and James Lee <diego@>
2016-11-28 11:19:08 -0800

    Check the cost of ship based on weight.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,20 +1,21 @@
 class Parcel
 
-  define_method(:initialize) do |length, width, height, maxDays, distance|
+  define_method(:initialize) do |length, width, height, maxDays, distance, weight|
     @length = length
     @width = width
     @height = height
     @maxDays = maxDays
     @distance = distance
+    @weight = weight
   end
 
   define_method(:volume) do
     @length * @width * @height
   end
   define_method(:cost) do
-    (volume * (1.6 - (@maxDays * 0.05)) * (0.99 + (@distance * 0.01)) * 0.6).round(2)
+    (volume * (1.6 - (@maxDays * 0.05)) * (0.99 + (@distance * 0.01)) *(1+((1-@weight) * 0.035)) * 0.6).round(2)
   end
 end
 # a = Parcel.new(:length=>3,:width=>3,:height=>3,:maxDays=>20)
 # puts(a.cost())
-# (volume * (1.6 - (maxDays * 0.05)) * (.99 + (distance * 0.01) * 0.6)
+# (volume * (1.6 - (maxDays * 0.05)) * (.99 + (distance * 0.01) *(1+((1-weight) * .035) * 0.6)
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,19 +3,23 @@ require('parcels')
 
 describe('Parcel#check_price') do
   it('calculate the volume') do
-    new_parcel = Parcel.new(3,3,3,20,1)
+    new_parcel = Parcel.new(3,3,3,20,1,1)
     expect(new_parcel.volume()).to(eq(27))
   end
   it('calculate the cost of the ship based on speed of delivery') do
-    new_parcel = Parcel.new(3,3,3,5,1)
+    new_parcel = Parcel.new(3,3,3,5,1,1)
     expect(new_parcel.cost()).to(eq(21.87))
   end
   it('calculate the cost of ship based on distance') do
-    new_parcel = Parcel.new(3,3,3,5,20)
+    new_parcel = Parcel.new(3,3,3,5,20,1)
     expect(new_parcel.cost()).to(eq(26.03))
   end
   it('calculate the cost of ship based on dimensions') do
-    new_parcel = Parcel.new(4,5,6,2,12)
+    new_parcel = Parcel.new(4,5,6,2,12,1)
     expect(new_parcel.cost()).to(eq(119.88))
+  end
+  it('calculate the cost of ship based on weight') do
+    new_parcel = Parcel.new(4,5,6,2,12,3)
+    expect(new_parcel.cost()).to(eq(111.49))
   end
 end
